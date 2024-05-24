9f3c85a29ea75ec222fdd7f1e22707586dadb8c8
kunnur123 <123663667+kunnur123@users.noreply.github.com>
2024-05-24 20:43:46 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "test" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "test-demo"
+  }
+}
