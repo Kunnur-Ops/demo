08dcd04384e2b1f152712f84a32ca9c9093a7811
kunnur123 <123663667+kunnur123@users.noreply.github.com>
2024-05-26 09:18:09 +0000

    updated terraform files

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,34 +1,22 @@
 terraform {
-   required_providers {
-      opentelekomcloud = {
-         source = "opentelekomcloud/opentelekomcloud"
-         version = ">= 1.23.2"
-      }
-   }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "opentelekomcloud_vpc_v1" "vpc_1" {
-  name = var.vpc_name
-  cidr = var.vpc_cidr
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
 }
 
-resource "opentelekomcloud_vpc_subnet_v1" "subnet_1" {
-  name       = var.subnet_name_1
-  cidr       = var.subnet_cidr_1
-  gateway_ip = var.subnet_gateway_ip_1
-  vpc_id     = opentelekomcloud_vpc_v1.vpc_1.id
-}
+resource "aws_instance" "test" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
-resource "opentelekomcloud_vpc_subnet_v1" "subnet_2" {
-  name       = var.subnet_name_2
-  cidr       = var.subnet_cidr_2
-  gateway_ip = var.subnet_gateway_ip_2
-  vpc_id     = opentelekomcloud_vpc_v1.vpc_1.id
-}
-
-resource "opentelekomcloud_vpc_subnet_v1" "subnet_3" {
-  name       = var.subnet_name_3
-  cidr       = var.subnet_cidr_3
-  gateway_ip = var.subnet_gateway_ip_3
-  vpc_id     = opentelekomcloud_vpc_v1.vpc_1.id
+  tags = {
+    Name = "test-demo"
+  }
 }
--- terraform-files/variables.tf
+++ terraform-files/variables.tf
@@ -1,54 +1,14 @@
-variable "vpc_name" {
-  description = "The subnet name. The value is a string of 1 to 64 characters that can contain letters, digits, underscores (_), and hyphens (-)"
+variable "region" {
+  description = "The AWS region to deploy to"
   type        = string
 }
 
-variable "vpc_cidr" {
-  description ="Specifies the network segment on which the subnet resides. The value must be in CIDR format. The value must be within the CIDR block of the VPC"
+variable "ami" {
+  description = "The AMI ID to use for the instance"
   type        = string
 }
 
-variable "subnet_name_1" {
-  description = "Specifies an array of one or more subnets associating with the route table"
-  type        = string
-}
-
-variable "subnet_name_2" {
-  description = "Specifies an array of one or more subnets associating with the route table"
-  type        = string
-}
-
-variable "subnet_name_3" {
-  description = "Specifies an array of one or more subnets associating with the route table"
-  type        = string
-}
-
-variable "subnet_cidr_1" {
-  description ="Specifies the network segment on which the subnet resides"
-  type        = string
-}
-
-variable "subnet_cidr_2" {
-  description ="Specifies the network segment on which the subnet resides"
-  type        = string
-}
-
-variable "subnet_cidr_3" {
-  description ="Specifies the network segment on which the subnet resides"
-  type        = string
-}
-
-variable "subnet_gateway_ip_1" {
-  description ="Specifies the gateway of the subnet.The value must be an IP address in the subnet segment"
-  type        = string
-}
-
-variable "subnet_gateway_ip_2" {
-  description ="Specifies the gateway of the subnet.The value must be an IP address in the subnet segment"
-  type        = string
-}
-
-variable "subnet_gateway_ip_3" {
-  description ="Specifies the gateway of the subnet.The value must be an IP address in the subnet segment"
+variable "instance_type" {
+  description = "The type of instance to create"
   type        = string
 }
